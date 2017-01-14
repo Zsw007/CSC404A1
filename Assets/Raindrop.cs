22b7a99095d93abd7ad79bc650782578f2ca6b90
Daniel Son <danielsonjh@hotmail.com>
2017-01-14 14:16:27 -0500

    Raindrops and controller.

---
+++ Assets/Raindrop.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Raindrop : MonoBehaviour
+{
+    public const float AdhesionK = 1f;
+    public const float CohesionK = 1f;
+    public const float MergeThreshold = 0.1f;
+
+    private Rigidbody _rigidbody;
+
+	void Start ()
+	{
+	    _rigidbody = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate ()
+	{
+		_rigidbody.AddForce(new Vector3(0, Mathf.Min(GetAdhesionForce() + _rigidbody.mass * Physics.gravity.y, 0), 0));
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.attachedRigidbody && other.CompareTag("Raindrop"))
+        {
+            var dir = transform.position - other.transform.position;
+            if (Controller.Instance.AttachedRaindrop == gameObject || 
+                dir.magnitude < MergeThreshold && transform.localScale.x >= other.transform.localScale.x && Controller.Instance.AttachedRaindrop != other.gameObject)
+            {
+                transform.localScale += other.transform.localScale;
+                _rigidbody.mass += other.attachedRigidbody.mass;
+                Destroy(other.gameObject);
+                _rigidbody.velocity = Vector3.zero;
+            }
+            else
+            {
+                other.attachedRigidbody.AddForce(dir * CohesionK / dir.sqrMagnitude);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Window"))
+        {
+            _rigidbody.velocity = Vector3.zero;
+            transform.position = new Vector3(transform.position.x, transform.position.y, other.transform.position.z);
+        }
+    }
+
+    private float GetAdhesionForce()
+    {
+        return Mathf.Sqrt(_rigidbody.mass) * AdhesionK;
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public static Score Instance { get; private set; }
+
+    public int Value = 0;
+
+    private Text _scoreText;
+
+	void Awake ()
+	{
+	    Instance = this;
+	}
+
+    void Start()
+    {
+        _scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        _scoreText.text = "Score: " + Value;
+    }
+}
